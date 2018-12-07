2507ad22444b400794fde39ce8c7b91f587fa63e
OrigamiElephant <mcculleyezr@urbandaleschools.com>
2018-12-07 13:29:06 -0600

    This is my Second commit

---
+++ Github_VCS.py
@@ -0,0 +1,102 @@
+'''
+Each student has 10 lines of code to play with!
+
+
+
+
+
+
+-----------------------------------------------
+Student 1: This is my master branch (YEET)
+Full Name:
+Define Forking:
+Define Cloning:
+Define Branching:
+Define Committing:
+Define Merging:
+Define Pushing:
+Define Pull Request:
+-----------------------------------------------
+Student 2:
+Full Name:
+Define Forking:
+Define Cloning:
+Define Branching:
+Define Committing:
+Define Merging:
+Define Pushing:
+Define Pull Request:
+-----------------------------------------------
+Student 3:
+Full Name:
+Define Forking:
+Define Cloning:
+Define Branching:
+Define Committing:
+Define Merging:
+Define Pushing:
+Define Pull Request:
+-----------------------------------------------
+Student 4:
+Full Name:
+Define Forking:
+Define Cloning:
+Define Branching:
+Define Committing:
+Define Merging:
+Define Pushing:
+Define Pull Request:
+-----------------------------------------------
+Student 5:
+Full Name:
+Define Forking:
+Define Cloning:
+Define Branching:
+Define Committing:
+Define Merging:
+Define Pushing:
+Define Pull Request:
+-----------------------------------------------
+Student 6:
+Full Name:
+Define Forking:
+Define Cloning:
+Define Branching:
+Define Committing:
+Define Merging:
+Define Pushing:
+Define Pull Request:
+-----------------------------------------------
+Student 7:
+Full Name:
+Define Forking:
+Define Cloning:
+Define Branching:
+Define Committing:
+Define Merging:
+Define Pushing:
+Define Pull Request:
+-----------------------------------------------
+Student 8:
+Full Name:
+Define Forking:
+Define Cloning:
+Define Branching:
+Define Committing:
+Define Merging:
+Define Pushing:
+Define Pull Request:
+-----------------------------------------------
+Student 9:
+Full Name:
+Define Forking:
+Define Cloning:
+Define Branching:
+Define Committing:
+Define Merging:
+Define Pushing:
+Define Pull Request:
+-----------------------------------------------
+
+
+'''
---
+++ README.md
@@ -0,0 +1,9 @@
+<img align="left" src="http://hermonswebsites.com/Classes/CS/python.png"><H2><a href="https://sites.google.com/urbandaleschools.com/pythonjedi/1-version-control" target="_blank">Chapter 1 Version Control Systems and Github</a></H2>
+
+
+
+In this chapter you will be practicing forking, cloning, branching, committing, merging, pushing and pull requesting. With the exception of pull requesting you will perform this workflow for each chapter throughout this class so it will be really important to have a solid understanding of this process. 
+
+<h3>Jedi Task</h3>
+
+You will be assigned 10 lines of code in the Github_VCS.py file in this repository to practice the VCS workflow. You will be completed with this Jedi Task when your instructor can respond to your pull request without any merging conflicts.
