c05e8f394ae8b5951ba60decd9d6efbdde4fc051
OrigamiElephant <mcculleyezr@urbandaleschools.com>
2018-12-10 13:13:59 -0600

    Finished definitions

--- Github_VCS.py
+++ Github_VCS.py
@@ -7,15 +7,15 @@ Each student has 10 lines of code to play with!
 
 
 -----------------------------------------------
-Student 1: This is my master branch
-Full Name:
-Define Forking:
-Define Cloning:
-Define Branching:
-Define Committing:
-Define Merging:
-Define Pushing:
-Define Pull Request:
+Student 1: Master branch
+Full Name: Ezra McCulley
+Define Forking: Copying a repository from someone else's account to my Github account
+Define Cloning: Copying a repository from my Github account down to my computer
+Define Branching: Create multiple lines of parallel code
+Define Committing: Saving with checkpoints that you could return to if you need
+Define Merging: Assimilating my branch back into my master branch
+Define Pushing: Send our commits out to our remote repository
+Define Pull Request: Asking Owner of original file to pull my code into the original file
 -----------------------------------------------
 Student 2:
 Full Name:
