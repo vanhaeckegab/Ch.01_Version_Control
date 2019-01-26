a045c0742ce736ffdc317639a792de364812f6ac
Marc Hermon <hermonm@urbandaleschools.com>
2019-01-25 18:17:24 -0600

    Update Github_VCS.py

--- Github_VCS.py
+++ Github_VCS.py
@@ -7,95 +7,105 @@ Each student has 10 lines of code to play with!
 
 
 -----------------------------------------------
-Student 1: Master branch
-Full Name: Ezra McCulley
-Define Forking: Copying a repository from someone else's account to my Github account
-Define Cloning: Copying a repository from my Github account down to my computer
-Define Branching: Create multiple lines of parallel code
-Define Committing: Saving with checkpoints that you could return to if you need
-Define Merging: Assimilating my branch back into my master branch
-Define Pushing: Send our commits out to our remote repository
-Define Pull Request: Asking Owner of original file to pull my code into the original file
+Student 1:
+
+
+
+
+
+
+
+
 -----------------------------------------------
 Student 2:
-Full Name: Aidan Kugley
-Define Forking: copying a repository from someone else's account to my github account
-Define Cloning: coping a repository from my github account down to my computer
-Define Branching: create multiple lines of parallel code
-Define Committing: saving with checkpoints that you could return to if you need
-Define Merging: injecting my branch back into the master branch
-Define Pushing: send our commits back to our remote repository
-Define Pull Request: asking the original repository owner to pull in our forked code
+
+
+
+
+
+
+
+
 -----------------------------------------------
 Student 3:
-Full Name: Eddie Agic
-Define Forking: Making a copy of the repository from someone else's account on to your account.
-Define Cloning: Making a copy of the repository from your account to your local machine.
-Define Branching: Creating multiple lines of parallel code.
-Define Committing: Saving with checkpoints that you could return to if you need. CMD+K
-Define Merging: Merging branches back into the master branch.
-Define Pushing: Sending commits out to our remote repository. SFT+CMD+K
-Define Pull Request: Asking the original repository owner to pull in our forked code.
+
+
+
+
+
+
+
+
 -----------------------------------------------
-Student 4: I am off the hook
-Full Name: Alexander Randall
-Define Forking: Copying a repository from someone else's account into your own
-Define Cloning: Copying the repository from the remote computer to the local computer
-Define Branching: Create Multiple Lines of parallel code
-Define Committing: Making a set of changes permanent/a checkpoint
-Define Merging: Merging my branch back into the master branch
-Define Pushing: Sending the file and its commits to the remote repository(Computer)
-Define Pull Request: Asking the first repository to pull our code into theirs
+Student 4:
+
+
+
+
+
+
+
+
 -----------------------------------------------
-Student 5: Varun
-Full Name: Varun Vepa
-Define Forking: taking a copy of someone else's repository and putting it on your account
-Define Cloning: making a copy of that repository on a local machine
-Define Branching: create multiple lines of parallel code
-Define Committing: saving with checkpoints that one can return to
-Define Merging: merging the test branch back into the master branch
-Define Pushing: sending our commits out to our remote repository
-Define Pull Request: asking the owner of the original repository owner to pull in the code that was tested
+Student 5:
+
+
+
+
+
+
+
+
 -----------------------------------------------
 Student 6:
-Full Name: noah mable
-Define Forking: copying a repository from someones else's account to my git hub account
-Define Cloning: copying a repository from my git hub account to my computer
-Define Branching: create multiple lines of parallel code
-Define Committing: saving with checkpoints that you can return to
-Define Merging: merging my branch back into the master branch
-Define Pushing: send our commits out to our remote repository
-Define Pull Request: asking the original repository owner to pull in our forked code˚
+
+
+
+
+
+
+
+
 -----------------------------------------------
 Student 7:
-Full Name: Jackson Rutto
-Define Forking: Copying a repository from someone else's account to my github account
-Define Cloning: Copying a repository from my github account to my computer
-Define Branching: Create multiple lines of parallel code
-Define Committing: Saving with checkpoints that you could return to if you need
-Define Merging: Merging my branch back into the master branch
-Define Pushing: Send our commits out to our remote repository
-Define Pull Request: Asking the original repository owner to pull in our forked code
+
+
+
+
+
+
+
+
 -----------------------------------------------
-Student 8: This is my master branch
-Full Name: Spencer Daves
-Define Forking: Copying a repository from someone else's account to my Github account
-Define Cloning: Copying a repository from my Github account down to my computer
-Define Branching: Create multiple lines of parallel code
-Define Committing: Saving with checkpoints that you could return to if you need
-Define Merging: Merging my branch back into the master branch
-Define Pushing: Send our commits out to our remote repository
-Define Pull Request: Asking the original repository owner to pull in our forked code
+Student 8:
+
+
+
+
+
+
+
+
 -----------------------------------------------
 Student 9:
-Full Name: Darius Brown
-Define Forking: Making a copy from one github account to yours
-Define Cloning: Copying the repository to my computer
-Define Branching: Making a new path that diverges from the master line
-Define Committing: Saving a checkpoint
-Define Merging: Converging the testing branch back into the master
-Define Pushing: uploading to repository
-Define Pull Request: asking to team leader to merge.
+
+
+
+
+
+
+
+
+-----------------------------------------------
+Student 10:
+
+
+
+
+
+
+
+
 -----------------------------------------------
 
 
