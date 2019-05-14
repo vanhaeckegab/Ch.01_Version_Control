c994f8175213fe9fae2da9fec0d4a051bb314b3a
Marc Hermon <hermonm@urbandaleschools.com>
2019-05-14 12:37:50 -0500

    Update and rename 1.0_Take_Home_Test.py to 1.0_Jedi_Tasks.py

---
+++ 1.0_Jedi_Tasks.py
@@ -0,0 +1,21 @@
+'''
+Sign your name:________________
+
+
+
+Define Forking: 
+
+Define Cloning: 
+
+Define Branching:
+
+Define Committing: 
+
+Define Merging: 
+
+Define Pushing:
+
+Define Pull Request:
+
+
+'''
