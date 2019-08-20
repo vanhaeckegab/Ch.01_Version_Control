2c9f7fc2469b8b037ea88c28c26ddb079de04f87
Marc Hermon <hermonm@urbandaleschools.com>
2019-08-19 23:35:40 -0500

    Create 1.2_turtle.py

---
+++ 1.2_turtle.py
@@ -0,0 +1,33 @@
+'''
+Modify the starter code below to create your own cool drawing
+and then Pull Request it to your instructor.
+
+Turtle Documentation: https://docs.python.org/3.3/library/turtle.html?highlight=turtle
+
+'''
+import turtle
+yoda=turtle.Turtle()
+screen=turtle.Screen() # makes a screen object
+screen.bgcolor('black') # colors the screen
+yoda.pensize(3) # width of pen line
+yoda.speed(10)  # speed of drawing. Go fast to not waste time.
+yoda.color("#00FF00")
+yoda.circle(100)  #head
+yoda.penup()
+yoda.setpos(50,185) #right ear
+yoda.pendown()
+yoda.goto(200,210)
+yoda.goto(88,145)
+yoda.penup()
+yoda.setpos(-50,185)  #left ear
+yoda.pendown()
+yoda.goto(-200,210)
+yoda.goto(-88,145)
+yoda.penup()
+yoda.setpos(200,-300)
+yoda.pendown()
+yoda.pencolor('#00FF00')
+yoda.write('Sign Your Name Here',font=("Arial", 12, "normal"))
+
+
+turtle.exitonclick() #Keeps pycharm window open
