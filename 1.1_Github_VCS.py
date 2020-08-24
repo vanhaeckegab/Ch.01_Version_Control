3d463a42bc214f3cf400c29c55d2b4b296adc16b
Marc Hermon <hermonm@urbandaleschools.com>
2020-08-24 15:46:33 -0500

    Update 1.1_Github_VCS.py
    Reset for 20-21

--- 1.1_Github_VCS.py
+++ 1.1_Github_VCS.py
@@ -1,36 +1,36 @@
 '''
 Each student has 10 lines of code to play with!
+For example, student #1 has lines 10-19
+and student #2 has lines 20-29.
+Be careful not to use anyone else's lines.
+Tell me some interesting facts about yourself.
+
+
+
+------------------Student 01------------------
 
 
 
 
-
-
-
-Rowdy Alexander
-I love Fishing
-
-yeet
-fish
 
 
 
 
 
 ------------------Student 02------------------
-This is the master branch
-Danny Halstead is Epic!
-Go Vikings!!!!!!!!!!!!!!
-Fishing is good
-I have no idea what to write
-I hope u enjoyed reading this
-Ten lines is a lot honestly
-I hope im doing this right
-This line has five words
+
+
+
+
+
+
+
+
+
 ------------------Student 03------------------
-This is the the master branch
-Mal Hmar
-Go jhawk!!!!
+
+
+
 
 
 
@@ -38,9 +38,9 @@ Go jhawk!!!!
 
 
 ------------------Student 04------------------
-This is the testing branch
-Steven Bui
-Favorite color is black
+
+
+
 
 
 
@@ -48,19 +48,19 @@ Favorite color is black
 
 
 ------------------Student 05------------------
-This is the master branch
-Peggy Y. Barley
-Go Jhawks
-I like moth orchids, riddles, and random facts
-Lemon meringue pie is the best type of pie
-I'm from Sweden, and I speak Swedish and English
-Solitaire, hanjie, and sudoku is actually really fun
-Horror movies/books are the best
-I really like the Joker and Pennywise, they seem fun
+
+
+
+
+
+
+
+
+
 ------------------Student 06------------------
-This is the testing branch
-Matthew Place
-Joining Military
+
+
+
 
 
 
@@ -68,29 +68,29 @@ Joining Military
 
 
 ------------------Student 07------------------
-Roses are red
-Coding is fun
-Lets do some coding
-Before class is done
--Tristan
+
+
+
+
+
 
 
 
 
 ------------------Student 08------------------
-This is the testing branch
-Catherine Clifford
-I have biology first period.
-attempt 2
-making a change
-fun fact
-currently adding more comments about myself
-attempt 3
-aksjdfhkjsahf
+
+
+
+
+
+
+
+
+
 ------------------Student 09------------------
-This is the testing branch
-Julie Pham
-i like purple and green
+
+
+
 
 
 
@@ -98,9 +98,9 @@ i like purple and green
 
 
 ------------------Student 10------------------
-This is the testing branch
-Nellie Leaverton
-ISU
+
+
+
 
 
 
@@ -108,25 +108,25 @@ ISU
 
 
 ------------------Student 11------------------
-This is the testing branch
-Cal Watson
-Where I want to go to college: The University of Oklahoma
-Intended Major: Meteorology
-Favorite Movie Saga: Star Wars
-Favorite TV Series: The Big Bang Theory
-If I could travel anywhere: Australia
-Favorite sport: Baseball
-Favorite music: Country
+
+
+
+
+
+
+
+
+
 ------------------Student 12------------------
-This is the testing branch
-Emma E. Moritz
-Go hawks!
-I weld
-Im not going to collage
-I don't enjoy school
-I enjoy your class
-I'm going to enjoy this class
-This year is going to be stressful
+
+
+
+
+
+
+
+
+
 ------------------Student 13------------------
 
 
@@ -138,11 +138,11 @@ This year is going to be stressful
 
 
 ------------------Student 14------------------
-Kenny Flory
-I need more sleep
-Show Choir is fun
-Iowa State Is Best Engineering School
-This class is fun
+
+
+
+
+
 
 
 
@@ -158,40 +158,40 @@ This class is fun
 
 
 ------------------Student 16------------------
-Caleb Hews
-Senior
-I am a member in Studio 2020
-I am helping Pizzazz and Vitality
-I play baseball
-I am planning on going to ISU
-I will be studying Aerospace Engineering
-This is my third and Final PLTW Course at the high school
+
+
+
+
+
+
+
+
 
 ------------------Student 17------------------
-Drake Kimmer
-Senior
-Love watching tv shows
-I play soccer
-Varsity Bowler
-hate school
-living life to the fullest
+
+
+
+
+
+
+
 
 
 ------------------Student 18------------------
-This is the master branch
-Lily Burkhead
-I like the bird, Raven
-I love playing Softball
-I'm excited for this class
+
+
+
+
+
 
 
 
 
 ------------------Student 19------------------
-Roses are red
-Violets are blue
-My name is not Bob
-and neither is yours
+
+
+
+
 
 
 
